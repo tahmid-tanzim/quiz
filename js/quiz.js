b5d625d659e92591836c89ecf8295a2665f47334
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2016-01-04 15:55:43 +0600

    Fine-Tuning Feedback

--- js/quiz.js
+++ js/quiz.js
@@ -36,5 +36,9 @@
             return $scope.myQuestions[qIndex].correctAnswer === aIndex;
         };
 
+        $scope.selectContinue = function () {
+            return $scope.activeQuestion += 1;
+        };
+
     }]);
 })();
